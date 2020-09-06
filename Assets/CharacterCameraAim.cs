99c2eb07563107c36a20489a5c1475130c778e03
gevonstria <gevtria@gmail.com>
2020-09-07 04:25:11 +0800

    Weapon Aiming Done

--- Assets/CharacterCameraAim.cs
+++ Assets/CharacterCameraAim.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class CharacterCameraAim : MonoBehaviour
 {
     public float turnSpeed = 15;
+    public float aimDuration = 0.3f;
+    public Rig aimLayer;
     Camera mainCamera;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +23,13 @@ public class CharacterCameraAim : MonoBehaviour
     {
         float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.fixedDeltaTime);
+    }
+
+    private void Update(){
+        if(Input.GetMouseButton(1)){
+            aimLayer.weight += Time.deltaTime / aimDuration;
+        }else{
+            aimLayer.weight -= Time.deltaTime / aimDuration;
+        }
     }
 }
